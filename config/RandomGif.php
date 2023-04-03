466329a6c86ef54c2ea0b0ff336dd9643a8611df
a.nikitin <a.nikitin@mail.business.ru>
2023-04-04 01:28:28 +0500

    Правки 7

---
+++ config/RandomGif.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+class RandomGif
+{
+    public static function url(string $name): string
+    {
+        $randomUrl = random_int(1, 2);
+        $urlDamir = match ($randomUrl) {
+            1 => 'https://giphy.com/gifs/tlou-stariylululul-NCMbaQhkPVXfhlcnbI',
+            2 => 'https://giphy.com/gifs/mads-laOHbXBwEc1aa3SFP4',
+        };
+
+        $urlTester = match ($randomUrl) {
+            1 => 'bruh',
+            2 => 'tester1',
+        };
+        return match ($name) {
+            'Damir' => $urlDamir,
+            'Tester' => $urlTester
+        };
+    }
+}
--- run.php
+++ run.php
@@ -8,8 +8,10 @@ use Discord\Parts\Channel\Message;
 use Discord\Parts\WebSockets\VoiceStateUpdate;
 use Discord\WebSockets\Event;
 use Discord\WebSockets\Intents;
+use Enum\RoleUser;
 use React\EventLoop\Loop;
 
+#var_dump($_ENV['DISCORD_TOKEN']);die();
 $ds = new Discord([
     'token' => $_ENV['DISCORD_TOKEN'],
     'loadAllMembers' => true,
@@ -18,21 +20,38 @@ $ds = new Discord([
     // Понадобится для отслеживания событий участников
 ]);
 
-$ds->on('init', static function (Discord $ds) {
-    $ds->on(Event::MESSAGE_CREATE, static function (Message $message, Discord $ds) {
-        // ... handle message sent
-    });
+$ds->on('ready', function (Discord $ds) {
+    echo "Bot is ready!", PHP_EOL;
 
+    // Listen for messages.
+//    $ds->on(Event::MESSAGE_CREATE, function (Message $message, Discord $discord) {
+//        echo "{$message->author->username}: {$message->content}", PHP_EOL;
+//        // Note: MESSAGE_CONTENT intent must be enabled to get the content if the bot is not mentioned/DMed.
+//    });
+    $ds->on(Event::VOICE_STATE_UPDATE, static function (VoiceStateUpdate $vSUpdate, Discord $ds) {
+        var_dump(
+            [
+                'channelWorkID: ' => $vSUpdate->channel_id,
+                '!is_null?' => !is_null($vSUpdate->channel_id)
+            ]
+        );
+        $userID = $vSUpdate->user->id;
+
+        $role = Roles::roleByUserID($userID);
+        $name = RoleUser::from($userID);
+
+
+
+        # User и войс канал куда заходит чел IT
+        if ($vSUpdate->user->id === '368107244199346176' && !is_null($vSUpdate->channel_id) && $vSUpdate->channel_id !== '1034416760415342684') {
+            $builder = MessageBuilder::new();
+            $channel = $vSUpdate->channel;
+            # Чат куда приходит письмо
+            $channel->id = '274886275176202240';
+            $url = RandomGif::url($name->name);
+            $channel->sendMessage($builder->setContent($role . $url));
+        }
+    });
 });
-$ds->on(Event::VOICE_STATE_UPDATE, static function (VoiceStateUpdate $vSUpdate, Discord $ds) {
-    print_r('channelWorkID: ' . $vSUpdate->channel_id);
-    # User и войс канал куда заходит чел IT
-    if ($vSUpdate->user->id === '368107244199346176' && $vSUpdate->channel_id === '1034416760415342684') {
-        $builder = MessageBuilder::new();
-        $channel = $vSUpdate->channel;
-        # Чат куда приходит письмо
-        $channel->id = '274886275176202240';
-        $channel->sendMessage($builder->setContent('<@368107244199346176> bruh'));
-    }
-});
+
 $ds->run();
