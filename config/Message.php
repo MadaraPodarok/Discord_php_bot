0e543adbaa186059e9816fb93dc50465d7127a52
a.nikitin <a.nikitin@mail.business.ru>
2023-04-05 13:47:23 +0500

    Правки 17

--- config/Message.php
+++ config/Message.php
@@ -5,16 +5,20 @@ namespace Config;
 use Discord\Builders\MessageBuilder;
 use Discord\Http\Exceptions\NoPermissionsException;
 use Discord\Parts\WebSockets\VoiceStateUpdate;
+use RuntimeException;
 
 class Message
 {
+    private const channelMessageChat = '274886275176202240';
+    private const channelMessageTest = '1092737067252990002';
+
     /**
      * @throws NoPermissionsException
      */
     public static function send(string $userID, ?VoiceStateUpdate $voiceStateUpdate = null): void
     {
         if (is_null($voiceStateUpdate)) {
-            throw new \RuntimeException('$voiceStateUpdate не найден');
+            throw new RuntimeException('$voiceStateUpdate не найден');
         }
         $role = Roles::roleByUserID($userID);
         $name = Roles::nameByUserID($userID);
@@ -23,8 +27,15 @@ class Message
         $builder = MessageBuilder::new();
 
         $channel = $voiceStateUpdate->channel;
+
+        if (getenv('APP_ENV') === 'production') {
+            $channelMessage = self::channelMessageChat;
+        } else {
+            $channelMessage = self::channelMessageTest;
+        }
+
         # Чат куда приходит письмо
-        $channel->id = '274886275176202240';
+        $channel->id = $channelMessage;
         $url = RandomGif::url($name);
         $channel->sendMessage($builder->setContent($role . ' ' . $url));
     }
