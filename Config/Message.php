0b55436c1fe6b3a0933af5f719af762d06c640f7
a.nikitin <a.nikitin@mail.business.ru>
2023-04-06 02:33:08 +0500

    Правки 26

---
+++ Config/Message.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Config;
+
+use Discord\Builders\MessageBuilder;
+use Discord\Discord;
+use Discord\Exceptions\FileNotFoundException;
+use Discord\Http\Exceptions\NoPermissionsException;
+use Discord\Parts\Channel\Channel;
+
+class Message
+{
+    private const channelMessageChat = '274886275176202240';
+    private const channelMessageTest = '1092737067252990002';
+
+    /**
+     * @throws NoPermissionsException
+     * @throws FileNotFoundException
+     */
+    public static function send(string $userID, Discord $discord, array $options): void
+    {
+        if (getenv('APP_ENV') === 'production') {
+            $channelMessage = self::channelMessageChat;
+        } else {
+            $channelMessage = self::channelMessageTest;
+        }
+
+        /** @var Channel $channel */
+        $channel = $discord->getChannel($channelMessage);
+
+        $role = Roles::roleByUserID($userID);
+        $name = Roles::nameByUserID($userID);
+        $gif = RandomGif::gif($name, $options);
+
+        $builder = MessageBuilder::new();
+
+        $channel->sendMessage(
+            $builder
+                ->setContent($role . PHP_EOL)
+                ->addFile($gif)
+        );
+    }
+}
---
+++ Config/Roles.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Config;
+
+use Log\Log;
+
+class Roles
+{
+    public static function roleByUserID(string $userID): string
+    {
+        switch ($userID) {
+            case '386154772568473610':
+                return "<@386154772568473610>";
+            case '276034798009581569':
+                return "<@276034798009581569>";
+            case '274889678950367232':
+                return "<@274889678950367232>";
+            case '268277651599261697':
+                return "<@268277651599261697>";
+            case '248406110019518464':
+                return "<@248406110019518464>";
+            case '781546965112586271':
+                return "<@781546965112586271>";
+            case '911741107061276725':
+                return "<@911741107061276725>";
+            case '368107244199346176';
+            default:
+                return "<@368107244199346176>";
+        }
+    }
+
+    public static function nameByUserID(string $userID): string
+    {
+        switch ($userID) {
+            case '386154772568473610':
+                return "Damir";
+            case '276034798009581569':
+                return "Chingiz";
+            case '274889678950367232':
+                return "Egor";
+            case '268277651599261697':
+                return "Alex";
+            case '248406110019518464':
+                return "Timur";
+            case '781546965112586271':
+                return "Igor";
+            case '911741107061276725':
+                return "Anastasiya";
+            case '283618794272980992':
+                return 'Askar';
+            case '726837943741972570':
+                return 'Maxim';
+            case '262446334441684993':
+                return 'Ivan';
+            case '258640185246351360':
+                return 'Sergei';
+            case '368107244199346176';
+                return "Tester";
+        }
+        Log::sendLog('error', 'Чей userID? ' . $userID);
+        return 'Tester';
+    }
+}
